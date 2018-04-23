8138cc4c65096d4c790b7a915f7e231bb90c3bdd
Taiwo Akinosho <tiwiex@yahoo.com>
2018-04-23 08:40:32 +0100

    first comit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+    get 'stylish_portfolios/index'
+    get 'creatives/index'
+    get 'freelancers/index'
+    get 'new_ages/index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
